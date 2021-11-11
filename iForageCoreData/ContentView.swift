756786ec951e05be34fd4aa5d0b13c6797976e58
CloseBuyUK <86831782+CloseBuyUK@users.noreply.github.com>
2021-11-11 21:47:20 +0000

    Initial Commit

---
+++ iForageCoreData/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iForageCoreData
+//
+//  Created by Connor A Lynch on 11/11/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iForageCoreData/iForageCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  iForageCoreDataApp.swift
+//  iForageCoreData
+//
+//  Created by Connor A Lynch on 11/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct iForageCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
